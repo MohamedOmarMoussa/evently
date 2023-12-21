0b37dbdb74e3cd7ce398d49d69faf7265a05a96a
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2023-12-21 20:44:32 +0100

    uploadthing-ok

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -172,7 +172,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                     <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
                       <Image
                         src="/assets/icons/location-grey.svg"
-                        alt="location"
+                        alt="calendar"
                         width={24}
                         height={24}
                       />
